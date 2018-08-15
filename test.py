0bbff3f177616b39ce423f3f7434dfc64a7f6bc6
William Silversmith <william.silversmith@gmail.com>
2018-08-15 15:55:14 -0400

    test: added travis and tests for zero size arrays

--- test.py
+++ test.py
@@ -1,21 +1,38 @@
+import pytest
+
 import numpy as np
 import fpzip
 
-x = np.random.random_sample(size=(128, 128, 128, 3)).astype(np.float64)
-y = fpzip.compress(x)
-z = fpzip.decompress(y)
+def test_doubles():
 
-assert np.all(x == z)
+  x = np.random.random_sample(size=(128, 128, 128, 3)).astype(np.float64)
+  y = fpzip.compress(x)
+  z = fpzip.decompress(y)
 
-x = np.random.random_sample(size=(128, 128, 128, 3)).astype(np.float32)
-y = fpzip.compress(x)
-z = fpzip.decompress(y)
+  assert np.all(x == z)
 
-assert np.all(x == z)
+  x = np.random.random_sample(size=(0, 0, 0, 0)).astype(np.float64)
+  y = fpzip.compress(x)
+  z = fpzip.decompress(y)
 
-x = np.random.random_sample(size=(128, 128, 128)).astype(np.float32)
-y = fpzip.compress(x)
-z = fpzip.decompress(y)
-z = np.squeeze(z, axis=3)
+  assert np.all(x == z)  
 
-assert np.all(x == z)
+def test_floats():
+  x = np.random.random_sample(size=(128, 128, 128, 3)).astype(np.float32)
+  y = fpzip.compress(x)
+  z = fpzip.decompress(y)
+
+  assert np.all(x == z)
+
+  x = np.random.random_sample(size=(128, 128, 128)).astype(np.float32)
+  y = fpzip.compress(x)
+  z = fpzip.decompress(y)
+  z = np.squeeze(z, axis=3)
+
+  assert np.all(x == z)
+
+  x = np.random.random_sample(size=(0, 0, 0, 0)).astype(np.float32)
+  y = fpzip.compress(x)
+  z = fpzip.decompress(y)
+
+  assert np.all(x == z)
