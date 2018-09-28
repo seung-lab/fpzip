1a0b46a441d1a138e6f135883e996a78c3767ac6
William Silversmith <william.silversmith@gmail.com>
2018-09-28 18:07:55 -0400

    fix: missed the dummy package trick in pandas

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
 import os
 import setuptools
+from setuptools import Command
 import pkg_resources
 import sys
 
 from setuptools.command.build_ext import build_ext as _build_ext
 
-# Bypass numpy installation prerequisite issue: https://stackoverflow.com/a/21621689
+# Use the numpy installation strategy pandas uses
 class build_ext(_build_ext):
   def finalize_options(self):
     numpy_incl = pkg_resources.resource_filename('numpy', 'core/include')
 
     for ext in self.extensions:
-        if (hasattr(ext, 'include_dirs') and
-                numpy_incl not in ext.include_dirs):
-            ext.include_dirs.append(numpy_incl)
+      if (hasattr(ext, 'include_dirs') and \
+        numpy_incl not in ext.include_dirs):
+
+        ext.include_dirs.append(numpy_incl)
 
     _build_ext.build_extensions(self)
+
+class DummyBuildSrc(Command):
+    """ numpy's build_src command interferes with Cython's build_ext.
+    """
+    user_options = []
+
+    def initialize_options(self):
+        self.py_modules_dict = {}
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        pass
 
 
 join = os.path.join
@@ -33,7 +49,10 @@ sources = [ join(fpzipdir, 'src', x) for x in (
 sources += [ 'fpzip.cpp' ]
 
 setuptools.setup(
-  cmdclass={'build_ext':build_ext},
+  cmdclass={
+    'build_src': DummyBuildSrc,
+    'build_ext': build_ext,
+  },
   setup_requires=['pbr', 'numpy'],
   install_requires=['numpy'],
   extras_require={
