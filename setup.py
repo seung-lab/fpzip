65bac3b5d18201e82ada61a5aca81a4324c35ec6
William Silversmith <william.silversmith@gmail.com>
2018-09-28 17:37:12 -0400

    fix: try with install_requires

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ sources += [ 'fpzip.cpp' ]
 setuptools.setup(
   cmdclass={'build_ext':build_ext},
   setup_requires=['pbr', 'numpy'],
+  install_requires=['numpy'],
   extras_require={
     ':python_version == "2.7"': ['futures'],
     ':python_version == "2.6"': ['futures'],
