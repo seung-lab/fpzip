a368c38254e7ad58cdbe9b9ccd9461c28159d549
William Silversmith <william.silversmith@gmail.com>
2018-09-28 17:59:38 -0400

    fix: try the numpy installation strategy pandas uses

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 import os
 import setuptools
+import pkg_resources
 import sys
 
 from setuptools.command.build_ext import build_ext as _build_ext
 
 # Bypass numpy installation prerequisite issue: https://stackoverflow.com/a/21621689
 class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
+  def finalize_options(self):
+    numpy_incl = pkg_resources.resource_filename('numpy', 'core/include')
+
+    for ext in self.extensions:
+        if (hasattr(ext, 'include_dirs') and
+                numpy_incl not in ext.include_dirs):
+            ext.include_dirs.append(numpy_incl)
+
+    _build_ext.build_extensions(self)
+
 
 join = os.path.join
 fpzipdir = 'fpzip-1.2.0'
